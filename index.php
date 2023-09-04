8011424c88e49087c3d43310b2b4b7bb738b1566
sitaphpwp <sitaphpwp@gmail.com>
2023-09-04 13:36:30 +0600

    Strings

--- index.php
+++ index.php
@@ -1,132 +1,57 @@
 <?php
 
-//  function
 
-/**
- * code of block
- * 
- * syntax :
- * 
- * function function_name(){
- *  statment;
- * } 
- */
+// $str1 = 'Hello';
+// $str2 = "World";
+// $str3 = $str1 . " " .  $str2;
 
 
-
-//  example: 
-
-
-//  function defination 
-// function addtion()
-// {
-//     // echo "";
-//     // return 
-// }
+// $about = 'Hello Wolrd\'s';
+// echo $about;
+// echo $str3;
 
 
+// echo <<<END
+//     Hello 
+//     Wolrd
+//       Sylhet
+// \n
+// END;
 
-// function call 
 
-// addtion();
+// strlen 
+
+// $str = "Hello World";
+
+// $len = strlen($str);
+// echo $len;
 
 
-// function defination 
-// function addition()
-// {
-//     $a = 10;
-//     $b = 20;
-//     $output = $a + $b;
-//     echo $output;
-//     return $output;
-// }
+//  array 
 
-// addition();
-
-// function_exists()
-
-//  fanction calling / invkoing 
-
-// $output = addition();
-
-// if ($output == 30) {
-//     echo "Value is 30";
-// } else {
-//     echo "value is not 30";
-// }
+// is_array => if is it  an array?
+//  in_array => seraching 
 
 
 
-// echo $output;
-// var_dump($output);
+$cityName = array('Dhaka', 'Sylhet');
 
+$serach = in_array('Dhakgfhfghgfa', $cityName);
 
-// / conditionl functio 
-
-// $is_true = true;
-
-// function subtraction()
-// {
-//     echo 'Test';
-// }
-
-// if ($makefoo) {
-//     subtraction();
-// } else {
-//     function add()
-//     {
-//         echo "Hello";
-//     }
-// }
-
-
-//  function parameter & arguments 
-
-//  $name = "variable" arguments
-// $city = array('dhaka','sylhet);
-
-
-// function Addition($a, $b, $c) // arguments
-// {
-//     echo $output = $a + $b + $c;
-// }
-
-// Addition(10, 20, 30); //parameter
-
-
-// function addition($a, $b,  $c)
-// {
-//     echo $output =  $a + $b + $c;
-// }
-
-
-// addition(10, 20, 3);
-
-
-//  include_once()  // include 
-// require_once ()  // require 
-
-//require_once './functions.php';
-include_once './functions.php';
-include_once './db.php';
-
-
-aboutPHP() . "\n";
-echo PHP_EOL;
-
-$add = addition(10, 20, 10);
-echo PHP_EOL;
-$sub = subtraction(10, 20, 60);
-echo $sub;
-echo $add;
-
-echo PHP_EOL;
-
-echo "================================";
-echo "<br>";
-$city = cityName();
-foreach ($city as $value) {
-    echo  $value . "<br>";
+if ($serach) {
+    echo "found it";
+} else {
+    echo "Not found";
 }
 
-// print_r($city);
+
+
+if (is_array($cityName)) {
+
+    foreach ($cityName as  $value) {
+
+        echo $value;
+    }
+} else {
+    echo "not array";
+}
